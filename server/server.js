5f2a0f5f7c5ac405cefa3992c6b3d63ad380dab1
dave_chao <dave790602@gmail.com>
2018-11-24 01:15:04 +0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -5,9 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo}= require('./models/todo');
 var {User}= require('./models/user');
 
-var app = express();
 const port = process.env.PORT || 3000;
-
+var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
