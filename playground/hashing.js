ad9bfbef5272e93c5c8cb1cbe4a40f8d7cf20947
dave_chao <dave790602@gmail.com>
2018-12-04 22:15:36 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,38 +1,53 @@
 const {SHA256} = require('crypto-js'); 
 const jwt = require('jsonwebtoken'); 
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
+bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$u4oElqACBFWmUFr/iISBqe9Yu5cjHnBMw8lol7YVCUEDLVofzit1e';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+// var data = {
+// 	id: 10
+// };
 
-var data = {
-	id: 4
-};
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var token = {
-	data, 
-	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
 
-var resultHash = SHA256(JSON.stringify(data) + 'somesecret').toString()
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 
-if(resultHash == token.hash) {
-	console.log('Data was not changed');
-} else {
-	console.log('Data was changed. Do not trust!');
-}
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data, 
+// 	hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(data) + 'somesecret').toString()
+
+// if(resultHash == token.hash) {
+// 	console.log('Data was not changed');
+// } else {
+// 	console.log('Data was changed. Do not trust!');
+// }
